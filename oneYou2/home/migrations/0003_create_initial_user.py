2f2b9513f03a4b1d041cb6fb018dfb74c2fa2aa6
Matt Nicks <matt.nicks@methods.co.uk>
2018-02-05 13:48:35 +0000

    migration added to create initial user

---
+++ oneYou2/home/migrations/0003_create_initial_user.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0002_create_homepage'),
+    ]
+
+    operations = [
+        migrations.RunSQL("INSERT INTO auth_user (password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES ('pbkdf2_sha256$36000$qlpsKAFs143l$ssUrFvFEI9bv6HgYjQq2VwNKEuMiFjTIShQonEC++mQ=', '1', 'superadmin', 'superadmin', 'superadmin', 'matt.nicks@methods.co.uk', '1', '1', (SELECT getdate()));"),
+    ]
