2dfc1154b3edd757e5916eda9fa84b5bfd4c024c
Matt Nicks <matt.nicks@methods.co.uk>
2018-02-06 14:45:07 +0000

    create release data object

--- oneYou2/home/models.py
+++ oneYou2/home/models.py
@@ -6,4 +6,9 @@ from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
-    pass
+  pass
+
+class Release(models.Model):
+  release_name = models.CharField(max_length=255, unique=True)
+  release_time = models.DateTimeField(blank=True, null=True)
+  
