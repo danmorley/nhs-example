b071c74adc43e074ba077c72506719d1ab3c0a7b
Matt Nicks <matt.nicks@methods.co.uk>
2018-02-07 12:30:36 +0000

    first version of the publisher command

---
+++ oneYou2/home/management/commands/publisher.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+from django.core.management.base import BaseCommand
+
+from home.models import Release
+
+class Command(BaseCommand):
+  def handle(self, *args, **kwargs):
+    current_releases = Release.objects.all()
+    post_data = {}
+    post_data['uuids'] = []
+    for release in current_releases:
+      post_data['uuids'].append(release.uuid)
+    url = 'http://web-pre-prod:8000/api/v2/releases/'
+    response = requests.post(url, post_data)
+    serialized_data = response.text
+    data = json.loads(serialized_data)
+    print(data)
+
+  
