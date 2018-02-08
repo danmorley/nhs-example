f8254299ac95d1c851ed97d0853a23d2c7384556
David Crees <david.crees@nhs.net>
2018-02-08 13:10:40 +0000

    Added bootstrap grid + sass

--- frontend/website-client/src/App.js
+++ frontend/website-client/src/App.js
@@ -6,12 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h1>Blank website</h1>
         </p>
       </div>
     );
