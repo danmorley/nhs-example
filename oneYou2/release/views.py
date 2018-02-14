5da3fe40ad4964c555c2ec8831a36eae57a42601
Matt Nicks <matt.nicks@methods.co.uk>
2018-02-14 11:56:21 +0000

    enable the release end point to be accessed with a get

--- oneYou2/release/views.py
+++ oneYou2/release/views.py
@@ -10,10 +10,13 @@ from django.views.decorators.http import require_POST
 import json
 from datetime import datetime
 
-@require_POST
+# @require_POST
 @csrf_exempt
 def releases(request):
-  data = json.loads(request.body)
-  releases = Release.objects.exclude(uuid__in=data['uuids']).exclude(release_time__isnull=True).filter(release_time__lte=datetime.now())
-  return HttpResponse(json.dumps(query_set_to_dict(releases)), content_type="application/json")
+  if request.method == 'POST':
+    data = json.loads(request.body)
+    releases = Release.objects.exclude(uuid__in=data['uuids']).exclude(release_time__isnull=True).filter(release_time__lte=datetime.now())
+    return HttpResponse(json.dumps(query_set_to_dict(releases)), content_type="application/json")
 
+  releases = Release.objects.filter(release_time__lte=datetime.now())
+  return HttpResponse(json.dumps(query_set_to_dict(releases)), content_type="application/json")
