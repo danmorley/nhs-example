2a21834d8f9002537fe9bba62d1753349449a59c
dmorley <dan_morley@yahoo.com>
2018-02-21 17:15:48 +0000

    button styles

--- frontend/website-client/src/components/shelves/PlaceholderShelf.js
+++ frontend/website-client/src/components/shelves/PlaceholderShelf.js
@@ -9,6 +9,7 @@ class PlaceholderShelf extends Shelf {
       <div className={this.shelfClasses()}>
         <div className="col-sm-12 shelf__placeholder">
           <h2 className="shelf__header">Placeholder Shelf</h2>
+          <button className="button button--default">Have a Go</button>
           <p>This is a temporary placeholder for a shelf of type: {this.props.shelfType}</p>
         </div>
       </div>
