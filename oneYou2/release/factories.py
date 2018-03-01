cf2a5863c032cb555e8cde28a770071cceaac24e
Matt Nicks <matt.nicks@methods.co.uk>
2018-03-01 15:43:35 +0000

    add release_time to the release factory

--- oneYou2/release/factories.py
+++ oneYou2/release/factories.py
@@ -1,7 +1,7 @@
 from .models import Release
 
 
-def create_test_release(release_name="Test release"):
-  release = Release(release_name=release_name)
+def create_test_release(release_name="Test release", release_date=None):
+  release = Release(release_name=release_name, release_time=release_date)
   release.save()
   return release
