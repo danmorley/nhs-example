a3ab7da62d593a3d5c70113e51072038af5493be
David Crees <david.crees@nhs.net>
2018-03-19 11:33:01 +0000

    Clean up

--- frontend/website-client/src/components/shared/ShowMorePanel.js
+++ frontend/website-client/src/components/shared/ShowMorePanel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './show-more-panel.css';
-import { CSSTransitionGroup } from 'react-transition-group';
 import classNames from 'classnames';
 
 /**
@@ -24,34 +23,8 @@ class ShowMorePanel extends Component {
   componentDidMount() {
     // Load expanded state from session storage.
     const isExpanded = sessionStorage.getItem(this.storageKey) === 'Y';
-    // let containerHeight = 'auto';
-    // if (!isExpanded) {
-    //   containerHeight = this.firstChildHeight();
-    // }
     this.setState({ isExpanded: isExpanded, childrenToView: this.childrenToView(isExpanded) });
   }
-
-  // firstChildHeight() {
-  //   console.log('1111');
-  //   let containerHeight = 'auto';
-  //   const firstChildElem = this.contentElem.firstElementChild;
-  //   containerHeight = firstChildElem.clientHeight;
-  //
-  //   console.log('2222');
-  //
-  //   return containerHeight;
-  // }
-
-  // lastChildOffsetX() {
-  //   console.log('1111');
-  //   const lastChildElem = this.contentElem.lastElementChild;
-  //   debugger;
-  //   const lastChildOffset = lastChildElem.clientHeight;
-  //
-  //   console.log('2222', lastChildElem);
-  //
-  //   return lastChildOffset;
-  // }
 
   componentWillUnmount() {
     // Save expanded state to session storage.
@@ -60,81 +33,37 @@ class ShowMorePanel extends Component {
 
   doExpand() {
     this.setState({ isExpanded: true, childrenToView: this.childrenToView(true) });
-
-    // this.gridContainerElem.style.maxHeight = this.panelWrapperHeight(); // Using this delays transition start until after timeout.
-
-    // Allow time for the transition to complete before setting the state and re-rendering.
-    // setTimeout(() => {
-    //   this.contentElem.style.transitionDuration = '1s';
-    //   this.contentElem.style.maxHeight = '800px'; //`${this.contentElem.clientHeight}px`; //`${this.gridContentHeight}px`;
-    // }, 500);
   }
 
   doContract() {
-    // const firstChildHeight = firstChildHeight();
-    // this.containerElem.style.maxHeight = this.firstChildHeight; //`${this.childPanelHeight}px`;
-    // this.gridContainerElem.style.maxHeight = this.panelWrapperHeight(); // Using this delays transition start until after timeout.
-
-    // Allow time for the transition to complete before setting the state and re-rendering.
-    // setTimeout(() => {
     this.setState({ isExpanded: false, childrenToView: this.childrenToView(false) });
-
-    // }, 1);
   }
 
-  // recordChildHeight(elem, index, rowsToShow) {
-  //   if (index === 1 && elem) this.childPanelHeight = elem.clientHeight * rowsToShow;
-  // }
-
   setContainerElem(elem) {
-    console.log('Container elem is', elem);
+    // console.log('Container elem is', elem);
     this.containerElem = elem;
   }
 
   setContentElem(elem) {
-    console.log('Content elem is', elem);
+    // console.log('Content elem is', elem);
     this.contentElem = elem;
   }
 
-  componentDidUpdate() {
-    console.log('in componentDidUpdate', this.contentElem);
-    // if (this.contentElem) {
-    //   const firstChildElem = this.contentElem.firstElementChild;
-    //   if (this.state.containerHeight !== firstChildElem.clientHeight) {
-    //     this.setState({ containerHeight: `${firstChildElem.clientHeight}px` });
-    //   }
-    // }
-  }
-
-  // setGridContentHeight(elem) {
-  //   if (elem) this.gridContentHeight = elem.clientHeight;
-  // }
-  // contractedPanelHeight() {
-  //   return `${this.props.childRowHeight}px`;
-  // }
-
-  // containerPanelHeight() {
-  //   const { rowsToShow } = this.props;
-  //   if (rowsToShow === 0) return 'auto';
-  //   return this.state.isExpanded ? `${this.contentElem.clientHeight}px` : this.contractedPanelHeight(); //`${this.gridContainerHeight}px` : `${this.childPanelHeight}px`;
-  // }
-
   childrenToView(isExpanded) {
-    let { panels, rowsToShow } = this.props;
+    let { rowsToShow, children } = this.props;
 
     if (isExpanded || rowsToShow === 0) {
-      console.log('childrenToView', panels.length);
-      return panels.length;  // Display all children.
+      console.log('childrenToView', children.length);
+      return children.length;  // Display all children.
+
     } else {
-      // const childrenToView = words.filter(word => word.length > 6);
-      // debugger;
       if (this.contentElem) {
         const childElems = this.contentElem.children;
         const firstChild = childElems[0];
         const width = firstChild.clientWidth;
         const availableWidth = this.contentElem.clientWidth * rowsToShow;
         let childrenToView = availableWidth / width;
-        if (childrenToView > panels.length) childrenToView = panels.length;
+        if (childrenToView > children.length) childrenToView = children.length;
         console.log('Calculated children to view is', childrenToView);
         return childrenToView;
 
@@ -146,11 +75,11 @@ class ShowMorePanel extends Component {
   }
 
   shouldShowMoreLessButton() {
-    let { panels, rowsToShow } = this.props;
+    let { rowsToShow, children } = this.props;
     if (rowsToShow === 0) return false;
 
     const noOfChildrenWhenCollapsed = this.childrenToView(false);
-    return panels.length > noOfChildrenWhenCollapsed;
+    return children.length > noOfChildrenWhenCollapsed;
   }
 
   firstChildDimensions() {
@@ -159,7 +88,7 @@ class ShowMorePanel extends Component {
   }
 
   renderChildren() {
-    return React.Children.map(this.props.panels, (child, i) => {
+    return React.Children.map(this.props.children, (child, i) => {
       if (i < this.state.childrenToView) return child;
 
       // Return a hidden child.
@@ -171,9 +100,8 @@ class ShowMorePanel extends Component {
   }
 
   render() {
-    let { rowsToShow, childRowHeight, panels } = this.props;
+    let { rowsToShow, childRowHeight, children } = this.props;
 
-    // const shouldShowMoreLessButton = content.rows_to_show > 0 && this.gridContainerElem && this.gridContainerElem.clientHeight < this.gridContentHeight;
     const container =
       <div key="1" ref={(elem) => this.setContainerElem (elem)} className="show-more-panel">
         <div ref={elem => this.setContentElem(elem)} className="row show-more-content">
@@ -182,8 +110,7 @@ class ShowMorePanel extends Component {
       </div>
     ;
 
-    const showShowButtons = this.shouldShowMoreLessButton();
-    const buttons = (showShowButtons &&
+    const buttons = (this.shouldShowMoreLessButton() &&
       <div key="2" className="row" style={{justifyContent: 'center'}}>
         {this.state.isExpanded ? (
           <a onClick={this.doContract}>See less</a>
@@ -194,8 +121,8 @@ class ShowMorePanel extends Component {
     );
 
     const info =
-      <div key="3">
-        Rows to show: {rowsToShow}, Children: {panels.length}, To view: {this.state.childrenToView}, Collapsed capacity: {this.childrenToView(false)}
+      <div key="3" className="debug-info">
+        Rows to show: {rowsToShow}, Children: {children.length}, To view: {this.state.childrenToView}, Collapsed capacity: {this.childrenToView(false)}
       </div>
     ;
 
@@ -209,7 +136,6 @@ class ShowMorePanel extends Component {
 
 ShowMorePanel.propTypes = {
   rowsToShow: PropTypes.number.isRequired,
-  childRowHeight: PropTypes.number.isRequired
 };
 
 export default ShowMorePanel;
--- frontend/website-client/src/components/shelves/GridShelf.js
+++ frontend/website-client/src/components/shelves/GridShelf.js
@@ -5,9 +5,6 @@ import CmsComponentRegistry from '../CmsComponentRegistry';
 import styles from './grid-shelf.css';
 import ShowMorePanel from '../shared/ShowMorePanel';
 
-import sampleBgImage from './healthcheckup.png'; // Tell Webpack this JS file uses this image
-// import testImage from '../../assets/images/Trump2.jpg';
-
 import PlaceholderPanel from '../panels/PlaceholderPanel';
 import VideoTeaserPanel from '../panels/VideoTeaserPanel';
 import ImageTeaserPanel from '../panels/ImageTeaserPanel';
@@ -25,64 +22,6 @@ import InformationPanel from '../panels/InformationPanel';
  *  full_width
  */
 class GridShelf extends Component {
-  // constructor (props) {
-  //   super(props);
-  //   this.state = { isGridExpanded: false };
-  //   this.doExpand = this.doExpand.bind(this);
-  //   this.doContract = this.doContract.bind(this);
-  //   this.storageKey = `${this.props.id}_panelExpanded`;
-  // }
-  //
-  // componentDidMount() {
-  //   // Load expanded state from session storage.
-  //   const isGridExpanded = sessionStorage.getItem(this.storageKey) === 'Y';
-  //   this.setState({ isGridExpanded: isGridExpanded });
-  // }
-  //
-  // componentWillUnmount() {
-  //   // Save expanded state to session storage.
-  //   sessionStorage.setItem(this.storageKey, this.state.isGridExpanded ? 'Y' : 'N');
-  // }
-  //
-  // doExpand() {
-  //   this.gridContainerElem.style.transitionDuration = '1s';
-  //   this.gridContainerElem.style.maxHeight = `${this.gridContentHeight}px`;
-  //   // this.gridContainerElem.style.maxHeight = this.panelWrapperHeight(); // Using this delays transition start until after timeout.
-  //
-  //   // Allow time for the transition to complete before setting the state and re-rendering.
-  //   setTimeout(() => {
-  //     this.setState({ isGridExpanded: true });
-  //   }, 1200);
-  // }
-  //
-  // doContract() {
-  //   this.gridContainerElem.style.maxHeight = `${this.childPanelHeight}px`;
-  //   // this.gridContainerElem.style.maxHeight = this.panelWrapperHeight(); // Using this delays transition start until after timeout.
-  //
-  //   // Allow time for the transition to complete before setting the state and re-rendering.
-  //   setTimeout(() => {
-  //     this.setState({ isGridExpanded: false });
-  //   }, 1200);
-  // }
-  //
-  recordChildHeight(elem, index, rowsToShow) {
-    if (index === 0 && elem) this.childPanelHeight = elem.clientHeight * rowsToShow;
-    console.log('child height is', this.childPanelHeight);
-  }
-  //
-  // setGridContainerElem(elem) {
-  //   this.gridContainerElem = elem;
-  // }
-  //
-  // setGridContentHeight(elem) {
-  //   if (elem) this.gridContentHeight = elem.clientHeight;
-  // }
-  //
-  // panelWrapperHeight(rowsToShow) {
-  //   if (rowsToShow === 0) return 'auto';
-  //   return this.state.isGridExpanded ? `${this.gridContainerHeight}px` : `${this.childPanelHeight}px`;
-  // }
-
   render() {
     let { id, content, classNamePrefix, layout, variant } = this.props;
     let metaLayout = content.meta_layout || layout;
@@ -94,22 +33,19 @@ class GridShelf extends Component {
       const panelClassNamePrefix = panelInfo && panelInfo.classNamePrefix;
       const panelId = panel.value.field_id || panel.id;
       if (PanelClass) {
-        // return (<div key={i} ref={props.inputRef} className={panelClass}><PanelClass content={panel.value} id={panelId} classNamePrefix={panelClassNamePrefix}/></div>);
-        // return (<div key={i} ref={(elem) => this.recordChildHeight(elem, i, content.rows_to_show)} className={panelClass}><PanelClass content={panel.value} id={panelId} classNamePrefix={panelClassNamePrefix}/></div>);
         return (<div key={i} className={panelClass}><PanelClass content={panel.value} id={panelId} classNamePrefix={panelClassNamePrefix}/></div>);
       } else {
         return (<div key={i} className={panelClass}><PlaceholderPanel panelType={panel.type} id={panelId} classNamePrefix={panelClassNamePrefix}/></div>);
       }
     });
 
-    // const shouldShowMoreLessButton = content.rows_to_show > 0 && this.gridContainerElem && this.gridContainerElem.clientHeight < this.gridContentHeight;
-
-    console.log('before return - child height is', this.childPanelHeight);
     return (
       <Shelf id={id} classNamePrefix={classNamePrefix}>
         <div className="shelf__container container">
           <h2 className="shelf__header">{content.heading}</h2>
-          <ShowMorePanel rowsToShow={content.rows_to_show} panels={panels} />
+          <ShowMorePanel rowsToShow={content.rows_to_show}>
+            {panels}
+          </ShowMorePanel>
         </div>
       </Shelf>
     );
