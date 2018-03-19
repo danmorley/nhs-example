889c89713125411b4000b1e7371be88bd5c5af6a
David Crees <david.crees@nhs.net>
2018-03-19 19:24:31 +0000

    Added front end support for document title and meta tags.

--- frontend/website-client/src/components/Page.js
+++ frontend/website-client/src/components/Page.js
@@ -4,6 +4,7 @@ import CmsComponentRegistry from './CmsComponentRegistry';
 import Footer from './Footer';
 import PageHeader from './page-header/PageHeader';
 import PageStyles from './PageStyles';
+import DocumentMeta from 'react-document-meta';
 
 // Wildcard imports are not supported by Babel without babel-wildcard plugin.
 // Create react app template app hides away Babel, plugin can not be added. You must import
@@ -21,12 +22,12 @@ import HeadingBodyShelf from './shelves/HeadingBodyShelf';
 class Page extends Component {
 
   render() {
-    if (!this.props.content) {
+    if (!this.props.page) {
         return (<div>Loading</div>);
     }
 
-    let { site, content } = this.props;
-    let { title, body, page_theme, page_styles } = content;
+    let { site, page } = this.props;
+    let { title, body, page_theme, page_styles } = page;
     let { menu, footer } = site;
     let pageTheme = (page_theme && page_theme.class_name) || 'oneyou';
 
@@ -44,17 +45,43 @@ class Page extends Component {
       }
     });
 
+    const documentTitle = `${site.site_name} - ${page.title}`;
+
+    const meta = {
+      title: documentTitle,
+      description: page.meta.search_description,
+      meta: {
+        property: {
+          'og:title': documentTitle,
+          'og:description': 'to do',
+          'og:url': 'to do',
+          'og:image': 'to do',
+          'og:type': 'to do'
+        },
+        name: {
+          'twitter:url': 'to do',
+          'twitter:card': 'to do',
+          'twitter:site': 'to do',
+          'twitter:title': 'to do',
+          'twitter:description': 'to do',
+          'twitter:imgae': 'to do'
+        }
+      }
+    };
+    
     return (
-      <div className={`page-wrapper ${pageTheme}`}>
-        <PageStyles content={page_styles} />
-        <PageHeader navItems={menu} header={site.header}/>
-        <div className="page-content-wrapper">
-          <div className="page-content">
-            {shelves}
+      <DocumentMeta {...meta}>
+        <div className={`page-wrapper ${pageTheme}`}>
+          <PageStyles content={page_styles} />
+          <PageHeader navItems={menu} header={site.header}/>
+          <div className="page-content-wrapper">
+            <div className="page-content">
+              {shelves}
+            </div>
+            <Footer className="page-footer" content={footer} site={site}/>
           </div>
-          <Footer className="page-footer" content={footer} site={site}/>
         </div>
-      </div>
+      </DocumentMeta>
     );
   }
 }
