1ee027ea5e68d35e1981776e5d551163df8660df
David Crees <david.crees@nhs.net>
2018-03-20 12:31:51 +0000

    Corrected comment

---
+++ frontend/website-client/src/components/shelves/IframeShelf.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Shelf from './Shelf';
+import CmsComponentRegistry from '../CmsComponentRegistry';
+
+/**
+ *  iFrame Shelf is a simple shelf that can be used to
+ *  display external content in an iframe.
+ *
+ *  It expects the following properties:
+ *  - content
+ *  - styles (to be confirmed)
+ *
+ *  content: {
+ *    src: 'https://link.to.content/',
+ *    frameborder: '0',
+ *    scrolling: 'no',
+ *    field_id: 'hay-container'
+ *  }
+ */
+class IframeShelf extends Component {
+  render() {
+    let { id, content, classNamePrefix } = this.props;
+
+    return (
+      <Shelf id={id} classNamePrefix={classNamePrefix} variant={content.meta_variant}>
+        <iframe
+          id={id}
+          src={content.src}
+          frameBorder={content.frame_border || '0'}
+          scrolling={content.scrolling || 'auto'}
+          height={content.height || '300px'}
+          width={content.width || '100%'}
+          sandbox={content.sandbox}
+          seamless>
+        </iframe>
+      </Shelf>
+    );
+  }
+}
+
+CmsComponentRegistry.register('iframe_shelf', IframeShelf, 'iframe-shelf');
+
+export default IframeShelf;
