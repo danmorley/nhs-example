6ab7541b7765ba1bafefe8b99e8fc9827b53cbd8
Matt Nicks <matt.nicks@methods.co.uk>
2018-04-05 12:51:38 +0100

    add the divider shelf to the frontend

---
+++ frontend/website-client/src/components/shelves/DividerShelf.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Shelf from './Shelf';
+import CmsComponentRegistry from '../CmsComponentRegistry';
+import styles from './divider-shelf.css';
+
+/**
+ *  Divider Shelf is a simple shelf that can be used to
+ *  display a hr divider.
+ *
+ *  It expects the following properties:
+ *  - styles (to be confirmed)
+ *
+ */
+class DividerShelf extends Component {
+  render() {
+    let { id } = this.props;
+
+    return (
+      <Shelf id={id} layout={'no-padding'}>
+        <hr/>
+      </Shelf>
+    );
+  }
+}
+
+CmsComponentRegistry.register('divider', DividerShelf, 'divider-shelf');
+
+export default DividerShelf;
