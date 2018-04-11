d6e71ee9e219991be680ea80295402000976ebe6
David Crees <david.crees@nhs.net>
2018-04-11 15:26:08 +0100

    Added support for multiple CTA links

--- frontend/website-client/src/components/panels/InformationPanel.js
+++ frontend/website-client/src/components/panels/InformationPanel.js
@@ -5,7 +5,7 @@ import styles from './information-panel.css';
 import Panel from './Panel';
 import PropTypes from 'prop-types';
 import ImageUtils from './ImageUtils';
-import CtaLink from '../shared/CtaLink';
+import CtaLinks from '../shared/CtaLinks';
 import Image from '../Image';
 
 /**
@@ -28,11 +28,11 @@ class InformationPanel extends Component {
     return (
       <Panel id={content.panel_id || this.props.id} classNamePrefix={classNamePrefix} variant={content.meta_variant}>
         <div className={`${classNamePrefix}__info`}>
-          <Text tagName="h2" content={content.heading}  className={`${classNamePrefix}__heading`} />
+          <Text tagName="h2" content={content.heading} className={`${classNamePrefix}__heading`} />
           <div className={`${classNamePrefix}__text`}>
             <Text content={content.body} className={`${classNamePrefix}__body`} format="richtext"/>
           </div>
-          <CtaLink cta={content.cta} variant = 'button' />
+          <CtaLinks cta={content.cta} variant='button' />
         </div>
         <div className={`${classNamePrefix}__image`} style={backgroundImage}>
         </div>
