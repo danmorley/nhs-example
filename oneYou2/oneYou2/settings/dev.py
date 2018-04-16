fdb0ef729691eb03ee4fcc353efaa387223963c5
Andrew Kenyon <andrewkenyon123@gmail.com>
2018-04-16 10:25:35 +0100

    turn off debugging in production

--- oneYou2/oneYou2/settings/dev.py
+++ oneYou2/oneYou2/settings/dev.py
@@ -3,7 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENV != 'dev':
+    DEBUG = False
+else:
+    DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#agido7mm*z&+o!dll)erh11&0_$-(y2+kuk3)t7z@6icw&1g^'
