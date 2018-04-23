4559f67794110386508a29d04dca64a0183afac4
Matt Nicks <matt.nicks@methods.co.uk>
2018-04-23 16:54:26 +0100

    fully test PageStyles component

--- frontend/website-client/src/components/PageStyles.test.js
+++ frontend/website-client/src/components/PageStyles.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PageStyles from './PageStyles.js';
 
-it('renders without crashing', () => {
+it('renders without crashing with no content', () => {
     const div = document.createElement('div');
     ReactDOM.render(<PageStyles />, div);
+});
+
+
+it('renders without crashing with content', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<PageStyles content='width: 100%;'/>, div);
 });
