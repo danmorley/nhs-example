09d841c50a13569543fb465f6600e8b2fcbb4d02
Matt Nicks <matt.nicks@methods.co.uk>
2018-04-23 17:25:06 +0100

    fully test Image component

--- frontend/website-client/src/components/Image.test.js
+++ frontend/website-client/src/components/Image.test.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Image from './Image.js';
 
-it('renders without crashing', () => {
+it('renders without crashing with not content', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Image />, div);
+});
+
+
+it('renders without crashing with content', () => {
+    const imageSrc = 'http://localhost/test_image.png';
+    const imageTitle = 'test image';
+    const imageObject = {link: imageSrc, title: imageTitle};
+    const div = document.createElement('div');
+    const image = ReactDOM.render(<Image image={imageObject}/>, div);
+    expect(image._reactInternalFiber.child.stateNode.src).toEqual(imageSrc);
+    expect(image._reactInternalFiber.child.stateNode.alt).toEqual(imageTitle);
 });
