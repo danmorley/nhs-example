e8955c09ef467c7673c80894cefc329fbc32eb0c
Matt Nicks <matt.nicks@methods.co.uk>
2018-04-24 12:47:01 +0100

    expand VideoModal testing

--- frontend/website-client/src/components/VideoModal.test.js
+++ frontend/website-client/src/components/VideoModal.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import VideoModal from './VideoModal.js';
 
-it('renders without crashing', () => {
+it('renders without crashing with no content', () => {
     const div = document.createElement('div');
     ReactDOM.render(<VideoModal />, div);
+});
+
+it('renders without crashing with content', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<VideoModal video={{}} />, div);
 });
