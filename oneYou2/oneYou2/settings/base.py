96c2ab1ecc095e2e4cae2988d2801e54240d4deb
Andrew Kenyon <andrewkenyon123@gmail.com>
2018-04-27 14:50:32 +0100

    changed package.json

--- oneYou2/oneYou2/settings/base.py
+++ oneYou2/oneYou2/settings/base.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'oneyou/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
