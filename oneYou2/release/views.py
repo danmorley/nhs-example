a99100799ab710afdda9d5da7addbddab3208808
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-01 12:23:32 +0100

    fix pep8

--- oneYou2/release/views.py
+++ oneYou2/release/views.py
@@ -32,9 +32,10 @@ def release_html(request, site_name):
         uuid = 'current'
 
     index = FrontendVersion.get_html_for_version(frontend_id)
-    substituted_index = index.replace("/oneyou/public/static/css/", "/{}/version/css/{}/?file_name=".format(site_name, frontend_id))
-    substituted_index = substituted_index.replace("/oneyou/public/static/js/", "/{}/version/js/{}/?file_name=".format(site_name,
-                                                                                                        frontend_id))
+    substituted_index = index.replace("/oneyou/public/static/css/",
+                                      "/{}/version/css/{}/?file_name=".format(site_name, frontend_id))
+    substituted_index = substituted_index.replace("/oneyou/public/static/js/",
+                                                  "/{}/version/js/{}/?file_name=".format(site_name,frontend_id))
 
     if settings.CONTENT_STORE_ENDPOINT:
         content_store_endpoint = settings.CONTENT_STORE_ENDPOINT
