7d7164fce671548230da8f00142c08f9d68dac40
Abhivan <ch.abhivan@gmail.com>
2018-05-01 10:39:44 +0100

    Add PUBLIC_ENV at build time.

--- scripts/build-docker-images-phedct.sh
+++ scripts/build-docker-images-phedct.sh
@@ -11,5 +11,5 @@ source answers.txt
 set +o allexport
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -t "phedct/${CI_PROJECT_NAME}:${IMAGE_TAG}" .
+docker build -t "phedct/${CI_PROJECT_NAME}:${IMAGE_TAG}" --build-arg PUBLIC_ENV="$REACT_APP_PUBLIC_URL" .
 docker push "phedct/${CI_PROJECT_NAME}:${IMAGE_TAG}"
