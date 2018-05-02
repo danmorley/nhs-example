d5469bdd5f55f034e5322efac492e19cf761ce12
Andrew Kenyon <andrewkenyon123@gmail.com>
2018-05-02 15:22:26 +0100

    added password policy

--- oneYou2/oneYou2/settings/base.py
+++ oneYou2/oneYou2/settings/base.py
@@ -83,6 +83,23 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'oneYou2.urls'
 
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
