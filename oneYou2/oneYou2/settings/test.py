1d815e95594a884c9f59ea91d278c4303ed6e98f
Andrew Kenyon <andrewkenyon123@gmail.com>
2018-05-14 10:15:45 +0100

    added some unit tests for images, more to come

--- oneYou2/oneYou2/settings/test.py
+++ oneYou2/oneYou2/settings/test.py
@@ -1,6 +1,11 @@
-from django.core.files.storage import FileSystemStorage
 from .base import *
+
+try:
+    from .local import *
+except ImportError:
+    pass
 
 SECRET_KEY = '#agido7mm*z&+o!dll)erh11&0_$-(y2+kuk3)t7z@6icw&1g^'
 
-DEFAULT_FILE_STORAGE = FileSystemStorage
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
