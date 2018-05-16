1549d4b95e186d1e47fafb8783c51c8916a7f12d
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 20:27:40 +0100

    expand OneYouTeaserPanel testing

--- frontend/website-client/src/components/panels/Oneyou1TeaserPanel.test.js
+++ frontend/website-client/src/components/panels/Oneyou1TeaserPanel.test.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Oneyou1TeaserPanel from './Oneyou1TeaserPanel';
+import { shallow } from 'enzyme';
 
 describe('Oneyou1TeaserPanel', () => {
+  const cta = [{link_external: 'https://dummyurl.com'}];
+
   let content = {
     image: 'test',
   }
-  let classNamePrefix = 'test'
+
+  let content_update = {
+    image: 'update',
+  };
+
+  let classNamePrefix = 'test';
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Oneyou1TeaserPanel content={content} classNamePrefix={classNamePrefix} />, div)
-  })
+  });
+
+  it('renders without crashing with a CTA', () => {
+    const div = document.createElement('div');
+    content.cta = cta;
+    ReactDOM.render(<Oneyou1TeaserPanel content={content} classNamePrefix={classNamePrefix} />, div)
+  });
+
+  it('unmounts without crashing', () => {
+    const wrapper = shallow(<Oneyou1TeaserPanel content={content} classNamePrefix={classNamePrefix} />);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.unmount();
+  });
+
+  it('it can receive new props without crashing', () => {
+    const wrapper = shallow(<Oneyou1TeaserPanel content={content} classNamePrefix={classNamePrefix} />);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.setProps({ content_update });
+  });
 })
