7afceb529ee5adf00976afd1c7bfe01446edcb0d
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 17:48:13 +0100

    correct formatting

--- frontend/website-client/src/components/shelves/PanelCarouselShelf.test.js
+++ frontend/website-client/src/components/shelves/PanelCarouselShelf.test.js
@@ -17,11 +17,12 @@ describe('PanelCarouselShelf', () => {
               "title": "10msu banner",
               "link": "https://campaignstorage.blob.core.windows.net/oneyou-cms-integration/original_images/2f86b9dd5ae29f46db9c66e3c89dfab1.jpg"
             },
-          "meta_variant": "dark-bg",
-          "cta": [],
-          "field_id": ""
-        },
-        "id": "cd8878f0-ff2b-4442-84b2-24541a2b589f"}
+            "meta_variant": "dark-bg",
+            "cta": [],
+            "field_id": ""
+          },
+          "id": "cd8878f0-ff2b-4442-84b2-24541a2b589f"
+        }
       ],
       field_id: "learn-more-shelf"
     }
