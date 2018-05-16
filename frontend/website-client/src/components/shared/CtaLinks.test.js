72d2ecad68ef1c34febff4c8bc30da4931b06782
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 18:50:31 +0100

    expand CtaLinks testing

--- frontend/website-client/src/components/shared/CtaLinks.test.js
+++ frontend/website-client/src/components/shared/CtaLinks.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CtaLinks from './CtaLinks';
+import {
+  MemoryRouter
+} from 'react-router-dom';
 
 describe('CtaLinks', () => {
   it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<CtaLinks />, div);
+    const cta = {link_page: {relative_path: '/dummyurl'}};
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLinks cta={cta} /></MemoryRouter>, div);
   });
 })
