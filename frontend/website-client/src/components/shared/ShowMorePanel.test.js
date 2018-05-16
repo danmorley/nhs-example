8ba1f997116b03df00c0555f8ed1e846d77c09ff
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 12:08:44 +0100

    fix showmore smoke test

--- frontend/website-client/src/components/shared/ShowMorePanel.test.js
+++ frontend/website-client/src/components/shared/ShowMorePanel.test.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import ShowMorePanel from './ShowMorePanel';
 
 describe('ShowMorePanel', () => {
+  let children = [];
+  let rowsToShow = 0;
   it('renders without crashing', () => {
       const div = document.createElement('div');
-      ReactDOM.render(<ShowMorePanel />, div);
+      ReactDOM.render(<ShowMorePanel children={children} rowsToShow={rowsToShow} />, div);
   });
 })
