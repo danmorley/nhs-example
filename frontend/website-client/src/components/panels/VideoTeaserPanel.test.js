545f6ca48555f4c8c29b5bcbd325a668281f7bd8
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 19:44:56 +0100

    expand VideoTeaserPanel testing

--- frontend/website-client/src/components/panels/VideoTeaserPanel.test.js
+++ frontend/website-client/src/components/panels/VideoTeaserPanel.test.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VideoTeaserPanel from './VideoTeaserPanel';
+import { shallow } from 'enzyme';
 
 describe('VideoTeaserPanel', () => {
   let content = {
     image: 'test',
   };
+
+  let content_update = {
+    image: 'update',
+  };
+
   let classNamePrefix = 'test';
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<VideoTeaserPanel content={content} classNamePrefix={classNamePrefix}/>, div)
-  })
+    ReactDOM.render(<VideoTeaserPanel content={content} classNamePrefix={classNamePrefix}/>, div);
+  });
+
+  it('unmounts without crashing', () => {
+    const wrapper = shallow(<VideoTeaserPanel content={content} classNamePrefix={classNamePrefix}/>);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.unmount();
+  });
+
+  it('it can receive new props without crashing', () => {
+    const wrapper = shallow(<VideoTeaserPanel content={content} classNamePrefix={classNamePrefix}/>);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.setProps({ content_update });
+  });
 })
