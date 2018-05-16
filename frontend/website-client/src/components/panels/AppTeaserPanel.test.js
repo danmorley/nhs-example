0d9d8a581d064c3a8b3e1c822d91de6613a1e0ed
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 20:39:39 +0100

    expand AppTeaserPanel testing

--- frontend/website-client/src/components/panels/AppTeaserPanel.test.js
+++ frontend/website-client/src/components/panels/AppTeaserPanel.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppTeaserPanel from './AppTeaserPanel';
+import { shallow } from 'enzyme';
 
 describe('AppTeaserPanel', () => {
   let content = {
     image: 'test',
-  }
+  };
+
+  let content_update = {
+    image: 'update',
+  };
+
   let classNamePrefix = 'test'
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<AppTeaserPanel content={content} classNamePrefix={classNamePrefix}/>, div)
-  })
+  });
+
+  it('unmounts without crashing', () => {
+    const wrapper = shallow(<AppTeaserPanel content={content} classNamePrefix={classNamePrefix}/>);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.unmount();
+  });
+
+  it('it can receive new props without crashing', () => {
+    const wrapper = shallow(<AppTeaserPanel content={content} classNamePrefix={classNamePrefix}/>);
+    expect(wrapper.exists()).toBe(true);
+    wrapper.setProps({ content_update });
+  });
 })
