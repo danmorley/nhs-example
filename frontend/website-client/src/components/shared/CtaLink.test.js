65d722b7d2433813ffb5c719e4c1e96c92401127
Matt Nicks <matt.nicks@methods.co.uk>
2018-05-16 19:05:08 +0100

    expand CtaLink testing

--- frontend/website-client/src/components/shared/CtaLink.test.js
+++ frontend/website-client/src/components/shared/CtaLink.test.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CtaLink from './CtaLink';
+import {
+  MemoryRouter
+} from 'react-router-dom';
 
 describe('CtaLink', () => {
   it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<CtaLink />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<CtaLink />, div);
+  });
+
+  it('renders without crashing when a cta type is specified', () => {
+    const cta = {type: 'single', value: {link_page: {relative_path: '/dummyurl'}}};
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLink cta={cta} /></MemoryRouter>, div);
+  });
+
+  it('renders without crashing when an invalid cta is provided', () => {
+    const cta = {link_page: null};
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLink cta={cta} /></MemoryRouter>, div);
+  });
+
+  it('renders without crashing when an external link is provided', () => {
+    const cta = {link_external: 'https://dummyurl.com'};
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLink cta={cta} /></MemoryRouter>, div);
+  });
+
+  it('renders without crashing when appstore variant is provided', () => {
+    const cta = {link_external: 'https://dummyurl.com'};
+    const variant = 'appstore';
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLink cta={cta} variant={variant} /></MemoryRouter>, div);
+  });
+
+  it('renders without crashing when googleplay variant is provided', () => {
+    const cta = {link_external: 'https://dummyurl.com'};
+    const variant = 'googleplay';
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><CtaLink cta={cta} variant={variant} /></MemoryRouter>, div);
   });
 })
