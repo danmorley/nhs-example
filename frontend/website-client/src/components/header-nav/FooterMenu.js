f3679159415d103a75b48d9c277bada0bfcaf3ec
dmorley <dan_morley@yahoo.com>
2018-05-21 15:42:28 +0100

    restructured code

--- frontend/website-client/src/components/header-nav/FooterMenu.js
+++ frontend/website-client/src/components/header-nav/FooterMenu.js
@@ -2,39 +2,33 @@ import React, { Component } from 'react';
 import SimpleMenuItem from './SimpleMenuItem';
 import { Link } from 'react-router-dom';
 import styles from './footer-menu.css';
+import chunk from 'lodash.chunk';
 
 class FooterMenu extends Component {
   render() {
     if (!this.props.items) return null;
     // Render simple menu items only.
     
-    let itemCol1 = [];
-    let itemCol2 = [];
-    
-    this.props.items.map((item, i) => {
-      if (item.type === 'simple_menu_item') {
-        if (i <= 3) {
-          itemCol1.push((<SimpleMenuItem key={i} item={item} classNamePrefix="footer" />));
-        }  
-        else {
-          itemCol2.push ((<SimpleMenuItem key={i} item={item} classNamePrefix="footer" />));
-        }
-      } else {
-        return null;
-      }
-    });
+    const siteMapLink = {
+      value: { 
+        link_text: 'Sitemap',
+        link_external: global.rootUrl +"/sitemap", 
+        link_page: {} 
+      },
+      type: "simple_menu_item"
+    }
+
+    let items = this.props.items.concat(siteMapLink);
 
     return (
       <div className ="footer-nav">
-        <ul className ="footer-nav__col-1">
-          {itemCol1}
-        </ul>
-        <ul className ="footer-nav__col-2">
-          {itemCol2}
-          <li>
-            <Link to={global.rootUrl +"/sitemap"} className="footer-nav__link">Sitemap</Link>
-          </li>
-        </ul>
+        {chunk(items, 4).map((list, i) => {
+          return <ul className={`footer-nav__col-${i+1}`}>
+            {list.map((item, j) => {
+              return <SimpleMenuItem key={j} item={item} classNamePrefix="footer" />;
+            })}
+          </ul>
+        })}      
       </div>
     );
   }
