287ef7c932f0b33479be38fd181868f17fc2e827
dmorley <dan_morley@yahoo.com>
2018-06-06 14:21:00 +0100

    back to top button

--- frontend/website-client/src/components/BackToTopButton.js
+++ frontend/website-client/src/components/BackToTopButton.js
@@ -4,10 +4,50 @@ import PropTypes from 'prop-types';
 
 
 class BackToTopButton extends Component {
+  constructor(props){
+    super(props);
+    this.scrollToTop=this.scrollToTop.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.state = ({
+      windowHeight : window.screen.height
+    });
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+  
+  setBackToTopButton(elem) {
+    this.backToTopButton = elem;
+  }
+  
+  scrollToTop() {
+    if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+      window.scrollBy(0, -50);
+      requestAnimationFrame(this.scrollToTop);
+    }
+  }
+  
+  handleClick(e) {
+    this.scrollToTop();
+  }
+   
+  handleScroll(event) {
+    if (window.scrollY > (this.state.windowHeight + 10)) {
+      this.backToTopButton.classList.add('back-to-top--show');
+    }
+    else {
+      this.backToTopButton.classList.remove('back-to-top--show');
+    }
+  }
+  
   render() {
-    
     return (
-      <a href="#" className="back-to-top" title="back to top of page">Hello</a>  
+      <button className="back-to-top" aria-label="back to top of page" ref={(elem) => this.setBackToTopButton(elem)} onClick={this.handleClick.bind(this)}></button>  
     )
   }
 }
