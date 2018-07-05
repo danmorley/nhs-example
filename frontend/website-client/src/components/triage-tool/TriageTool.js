0e5bc3cb243c22c29c31c3ce4567b7e70f5106dd
David Crees <david.crees@nhs.net>
2018-07-05 17:45:04 +0100

    WIP added state updates to TriageTool component

--- frontend/website-client/src/components/triage-tool/TriageTool.js
+++ frontend/website-client/src/components/triage-tool/TriageTool.js
@@ -8,6 +8,19 @@ class TriageTool extends Component {
       currentPanel: 0,
       answers: []
     }
+
+    this.assessmentComplete = this.assessmentComplete.bind(this);
+    this.getContextComplete = this.getContextComplete.bind(this);
+  }
+
+  assessmentComplete() {
+    // Calculate dependency level from the assessment answers.
+    this.setState({currentPanel: 1, dependencyLevel: 'high'});
+  }
+
+  getContextComplete() {
+    // Store the user history.
+    this.setState({currentPanel: 2, userHistory: []});
   }
 
   render() {
@@ -16,13 +29,13 @@ class TriageTool extends Component {
     return (
       <div>
         {this.state.currentPanel === 0 &&
-          <Assess config={config.assessment.questions} />
+          <Assess config={config.assessment.questions} onComplete={this.assessmentComplete} />
         }
         {this.state.currentPanel === 1 &&
-          <GetContext config={config.getContext.questions} />
+          <GetContext config={config.getContext.questions} onComplete={this.getContextComplete} />
         }
         {this.state.currentPanel === 2 &&
-          <QuittingPlan config={config} dependencyLevel={} context={} />
+          <QuittingPlan config={config} dependencyLevel={this.state.dependencyLevel} context={this.state.userHistory} />
         }
       </div>
     );
