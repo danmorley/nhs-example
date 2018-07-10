d3ff1ceae30e19527f4afcb1f4e1d1663e75ba7b
dmorley <dan_morley@yahoo.com>
2018-07-10 13:09:26 +0100

    added Cta Panel tests

--- frontend/website-client/src/components/BrightcoveVideo.test.js
+++ frontend/website-client/src/components/BrightcoveVideo.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrightCoveVideo from './BrightCoveVideo.js';
+import BrightcoveVideo from './BrightcoveVideo.js';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BrightCoveVideo />, div);
+    ReactDOM.render(<BrightcoveVideo />, div);
 });
-
 it('renders without crashing with content', () => {
     const div = document.createElement('div');
     ReactDOM.render(<BrightCoveVideo video={"video_source"} />, div);
