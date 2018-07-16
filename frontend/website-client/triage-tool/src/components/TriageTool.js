fd825d7d2c356efd8d2a2ac0d9cd9a5e953cab21
Sikander Akbar Ali <sikander.ali@nhs.net>
2018-07-16 11:34:21 +0100

    add react icons to project

--- frontend/website-client/triage-tool/src/components/TriageTool.js
+++ frontend/website-client/triage-tool/src/components/TriageTool.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-//import { ChevronDown, ChevronUp } from "styled-icons/octicons"
+import { GoChevronUp, GoChevronDown } from "react-icons/lib/go"
 import { observer } from "mobx-react"
 
 import { TriageToolContainer, AppHeader, AppIntro, AccordionPanelContainer, Button, OpenIndicator } from "./styles"
@@ -67,7 +67,7 @@ class AccordionPanel extends Component {
     const smallHeader = (
       <div>
         { this.props.heading }
-      {/* <OpenIndicator>{ this.props.open ? <ChevronUp size="30" /> : <ChevronDown size="30" />}</OpenIndicator> */}
+        <OpenIndicator>{ this.props.open ? <GoChevronUp size="30" /> : <GoChevronDown size="30" />}</OpenIndicator>
       </div>
     )
     return (
