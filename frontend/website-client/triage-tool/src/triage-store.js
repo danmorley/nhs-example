ef34e0cf731fbfbb1c6b8c727ec5530ec3230d39
Daniel King <daniel.king5@nhs.net>
2018-07-17 16:59:52 +0100

    add logic for tracking triage tool answers
    Using mobx-state-tree to track what options are selected and derive dependence score.
    
    This change removes state from react components and consolidates business logic in the triage-store.js file.
    
    The feedback section of tool is displaying options selected and dependence value for debug during development.

--- frontend/website-client/triage-tool/src/triage-store.js
+++ frontend/website-client/triage-tool/src/triage-store.js
@@ -1,18 +1,66 @@
-import { types } from "mobx-state-tree"
+import { types, getParent } from "mobx-state-tree"
+import { xor } from "lodash"
+
+const Option = types
+  .model("Option", {
+    id: types.identifier(types.string),
+    text: types.string,
+    dependence: types.optional(types.number, 0)
+  })
+  .actions(self => ({
+    toggleSelect() {
+      getParent(getParent(self)).selectOption(self)
+    }
+  }))
+  .views(self => ({
+    get selected() {
+      return getParent(getParent(self)).isSelected(self.id.toString())
+    }
+  }))
+
+const Question = types
+  .model("Question", {
+    text: types.string,
+    inputType: types.enumeration("inputType", ["radio", "checkbox"]),
+    options: types.array(Option),
+    selectedOptions: types.optional(types.array(types.reference(Option)), [])
+  })
+  .actions(self => ({
+    selectOption(option) {
+      if (self.inputType == "radio") {
+        self.selectedOptions = [option]
+      } else {
+        self.selectedOptions = xor(self.selectedOptions, [option])
+      }
+    }
+  }))
+  .views(self => ({
+    isSelected(option) {
+      return self.selectedOptions.map(o => o.id).includes(option)
+    }
+  }))
 
 const TriageStore = types
   .model("TriageStore", {
-    currentPanel: types.number
+    currentPanel: types.number,
+    questions: types.array(Question)
   })
   .actions(self => ({
     changePanel(panel) {
       self.currentPanel = panel
     }
   }))
-
-//const Question = types
-//  .model("Question", {
-//    type: types.enumeration("questionType", ["radio", "checkbox"])
-//  })
+  .views(self => ({
+    get dependence() {
+      return self.questions
+        .reduce((acc, question) => acc.concat(question.selectedOptions), [])
+        .reduce((acc, option) => acc + option.dependence, 0)
+    },
+    get previousAttempts() {
+      return self.questions
+        .reduce((acc, question) => acc.concat(question.selectedOptions), [])
+        .reduce((acc, option) => acc + option.text, [])
+    }
+  }))
 
 export default TriageStore
