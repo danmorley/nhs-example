1a4c405f758a56ec16930dff4dd74c0138ff2dd3
Sikander Akbar Ali <sikander.ali@nhs.net>
2018-07-30 12:38:19 +0100

    add adobe tagging

--- oneYou2/frontendHandler/models.py
+++ oneYou2/frontendHandler/models.py
@@ -28,6 +28,7 @@ class FrontendVersion:
 
     @classmethod
     def get_available_versions(cls):
+        return []
         print('loading available versions')
 
         # TODO try to find a way to mock this function on server start
