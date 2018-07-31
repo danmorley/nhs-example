465befffb4c598866c422ff1c56c8aa7b9ff483c
Daniel King <daniel.king5@nhs.net>
2018-07-31 17:03:20 +0100

    add ecig and vapes to triage tool plan

--- frontend/website-client/triage-tool/src/components/Plan.js
+++ frontend/website-client/triage-tool/src/components/Plan.js
@@ -4,6 +4,7 @@ import { observer } from "mobx-react"
 
 import { planTemplates } from "../config"
 import FaceToFaceSupport from "./FaceToFaceSupport"
+import CurbCravings from "./CurbCravings"
 import { PlanContainer } from "./styles"
 
 class Plan extends Component {
@@ -20,6 +21,10 @@ class Plan extends Component {
         <div>
           <h2>Get face-to-face support</h2>
           <FaceToFaceSupport store={ store }></FaceToFaceSupport>
+        </div>
+        <div>
+          <h2>Curb your cravings with a combination</h2>
+          <CurbCravings store={ store }></CurbCravings>
         </div>
       </PlanContainer>
     )
