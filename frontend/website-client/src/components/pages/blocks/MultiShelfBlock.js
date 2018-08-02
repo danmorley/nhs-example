95a5911f3efe182c3016caffbb954795295f690e
Daniel King <daniel.king5@nhs.net>
2018-08-02 14:29:39 +0100

    remove triage tool shelf from multishelfblock

--- frontend/website-client/src/components/pages/blocks/MultiShelfBlock.js
+++ frontend/website-client/src/components/pages/blocks/MultiShelfBlock.js
@@ -26,7 +26,7 @@ import ArticlePageHeadingShelf from '../../shelves/ArticlePageHeadingShelf';
 import RecipeGridShelf from '../../shelves/RecipeGridShelf';
 import ShareButtonShelf from '../../shelves/ShareButtonShelf';
 import TableShelf from '../../shelves/TableShelf';
-import TriageToolShelf from '../../shelves/TriageToolShelf';
+//import TriageToolShelf from '../../shelves/TriageToolShelf';
 
 class MultiShelfBlock extends Component {
   render() {
