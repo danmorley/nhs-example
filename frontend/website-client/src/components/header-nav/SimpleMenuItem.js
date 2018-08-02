fc799b38c97f7780e09ffaf68d96da60e96c5c37
dmorley <dan_morley@yahoo.com>
2018-08-02 11:43:28 +0100

    allow html in nav titles

--- frontend/website-client/src/components/header-nav/SimpleMenuItem.js
+++ frontend/website-client/src/components/header-nav/SimpleMenuItem.js
@@ -6,11 +6,13 @@ import { Link } from 'react-router-dom';
 class SimpleMenuItem extends Component {
   render() {
     let { link_page, link_external, link_text } = this.props.item.value;
-
+    
     return (
       <li className = {this.props.classNamePrefix+"-nav__item"}>
         <div className= {this.props.classNamePrefix+"-nav__separator"}>
-          <Link onClick={this.handleClick} to={link_page.relative_path || link_external}  className = {this.props.classNamePrefix+"-nav__link"}>{link_text}</Link>
+          <Link onClick={this.handleClick} to={link_page.relative_path || link_external}  className = {this.props.classNamePrefix+"-nav__link"}>
+            <span dangerouslySetInnerHTML={{__html: link_text }} />
+          </Link>
         </div>
       </li>
     );
