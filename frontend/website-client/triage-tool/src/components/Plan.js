fc27f2d3e451bd2b251f3e6a8c4f9978618bcc26
Daniel King <daniel.king5@nhs.net>
2018-08-07 18:34:48 +0100

    refactor plan to config files

--- frontend/website-client/triage-tool/src/components/Plan.js
+++ frontend/website-client/triage-tool/src/components/Plan.js
@@ -2,17 +2,26 @@ import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { observer } from "mobx-react"
 
-import { planTemplates } from "../config"
+import { planTemplates, planSteps } from "../config"
 import FaceToFaceSupport from "./FaceToFaceSupport"
 import CurbCravings from "./CurbCravings"
+import PlanStep from "./PlanStep"
 import { PlanContainer } from "./styles"
 
 class Plan extends Component {
   render() {
     const store = this.props.store
+    const steps = planSteps.map((step, i) => {
+      return <PlanStep key={ i } step={ step } store={ store }></PlanStep>
+    })
 
     return (
       <PlanContainer>
+        { steps }
+        <hr />
+        <hr />
+        <hr />
+        <hr />
         <div>
           <h2>{ "Great you're quitting!"}</h2>
           <p>{ planTemplates.planIntro(store) }</p>
