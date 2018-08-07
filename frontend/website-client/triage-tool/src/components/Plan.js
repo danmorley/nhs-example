6e442268eef16c9e0c57bc057b966fdca64dd4d0
Daniel King <daniel.king5@nhs.net>
2018-08-07 18:41:17 +0100

    remove curbcravings and facetoface react components

--- frontend/website-client/triage-tool/src/components/Plan.js
+++ frontend/website-client/triage-tool/src/components/Plan.js
@@ -2,9 +2,7 @@ import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { observer } from "mobx-react"
 
-import { planTemplates, planSteps } from "../config"
-import FaceToFaceSupport from "./FaceToFaceSupport"
-import CurbCravings from "./CurbCravings"
+import { planSteps } from "../config"
 import PlanStep from "./PlanStep"
 import { PlanContainer } from "./styles"
 
@@ -18,23 +16,6 @@ class Plan extends Component {
     return (
       <PlanContainer>
         { steps }
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-        <div>
-          <h2>{ "Great you're quitting!"}</h2>
-          <p>{ planTemplates.planIntro(store) }</p>
-          <hr />
-        </div>
-        <div>
-          <h2>Get face-to-face support</h2>
-          <FaceToFaceSupport store={ store }></FaceToFaceSupport>
-        </div>
-        <div>
-          <h2>Curb your cravings with a combination</h2>
-          <CurbCravings store={ store }></CurbCravings>
-        </div>
       </PlanContainer>
     )
 
