73db78515542f3328db2ae181b391b42c9bf201e
Daniel King <daniel.king5@nhs.net>
2018-08-15 17:50:10 +0100

    WIP add post request for triage service finder

--- frontend/website-client/triage-tool/src/components/ServiceFinder.js
+++ frontend/website-client/triage-tool/src/components/ServiceFinder.js
@@ -3,20 +3,33 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { observer } from "mobx-react"
+import Autocomplete from "react-autocomplete"
 
 import { Button, ServiceFinderContainer } from "./styles"
 
 class ServiceFinder extends Component {
   constructor(props) {
     super(props)
-    this.state = { searchLocation: "", suggestions: [] }
+    const defaultLocation = {
+      label: "",
+      latitude: "",
+      longitude: ""
+    }
+
+    this.state = {
+      searchTerm: "",
+      searchLocation: defaultLocation,
+      suggestions: []
+    }
 
     this.suggestLocation = this.suggestLocation.bind(this)
+    this.findServices = this.findServices.bind(this)
+    this.setLocation = this.setLocation.bind(this)
   }
 
   suggestLocation(event) {
     const newLocation = event.target.value
-    this.setState({ searchLocation: newLocation })
+    this.setState({ searchTerm: newLocation })
     if (newLocation.length < 2) {
       this.setState({ suggestions: [] })
       return
@@ -34,29 +47,71 @@ class ServiceFinder extends Component {
 
     fetch(url, requestOptions).then(response => {
       response.json().then(data => {
-        const postcodes = data.value.map(r => r.Name1)
+        let postcodes = data.value.map(r => {
+          return {
+            label: r.Name1,
+            latitude: r.Latitude,
+            longitude: r.Longitude
+          }
+        })
+        console.dir(postcodes)
         this.setState({ suggestions: postcodes })
       })
     })
   }
 
-  render() {
-    const suggestions = this.state.suggestions.map((suggestion, i) => {
-      return <option key={ i } value={ suggestion }/>
-    })
+  findServices() {
+    console.log(this.state.searchLocation)
+    const { latitude, longitude } = this.state.searchLocation
 
+    const url = `https://nhsukpoc.search.windows.net/indexes/organisationlookup3-index/docs/search?api-version=2016-09-01`
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "api-key": process.env.NHS_FLEXIFINDER_API_KEY,
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        "search": "*",
+        "filter": "ServicesProvided/any(p: p eq 'Stop smoking support services')",
+        "orderby": `geo.distance(Geocode, geography'POINT(${longitude} ${latitude})')`,
+        "top": 20,
+        "count": true
+      })
+    }
+
+    fetch(url, requestOptions).then(response => {
+      response.json().then(data => {
+        console.dir(data)
+      })
+    })
+  }
+
+  setLocation(label, locationItem) {
+    this.setState({
+      searchTerm: label,
+      searchLocation: locationItem
+    })
+  }
+
+  render() {
     return (
       <ServiceFinderContainer>
-        <datalist id="service-finder-suggestions">
-          { suggestions }
-        </datalist>
-        <input
-          placeholder="Enter postcode"
-          list="service-finder-suggestions"
+        <Autocomplete
+          inputProps={{ placeholder: "Enter postcode" }}
           onChange={ this.suggestLocation }
-          value={ this.state.searchLocation }
+          onSelect={ this.setLocation }
+          value={ this.state.searchTerm }
+          getItemValue={ item => item.label }
+          items={ this.state.suggestions }
+          renderItem={ (item, isHighlighted) =>
+            <div style={{ background: isHighlighted ? "lightgray" : "white" }}>
+              { item.label }
+            </div>
+          }
         />
-        <Button>Find</Button>
+        <Button onClick={ this.findServices }>Find</Button>
       </ServiceFinderContainer>
     )
   }
