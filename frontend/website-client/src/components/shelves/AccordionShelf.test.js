aca5955c95b03fa904642b048856dad29dc40f44
Matt Nicks <matt.nicks@methods.co.uk>
2018-09-14 23:59:03 +0100

    add testing for accordion shelf

---
+++ frontend/website-client/src/components/shelves/AccordionShelf.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AccordionShelf from './AccordionShelf';
+
+describe('AccordionShelf', () => {
+  let content = {
+    tracking_group: "",
+    heading: "Accordion 1",
+    items: [
+      {
+        type: "rich_text_panel",
+        value: {
+          text: "<p>Accordion 1 content.</p><p>Roof party yr yuccie tofu raclette sriracha quinoa squid occupy gentrify pork belly irony artisan subway tile disrupt. Live-edge typewriter polaroid disrupt chia quinoa. Copper mug adaptogen chartreuse jianbing ennui. Roof party shoreditch keffiyeh, craft beer farm-to-table gochujang 8-bit slow-carb hexagon. Lo-fi artisan YOLO tilde, vice air plant woke kale chips semiotics hexagon shoreditch iPhone fixie heirloom hammock. La croix wayfarers tilde austin vegan readymade everyday carry polaroid. Chicharrones stumptown tousled bicycle rights freegan la croix butcher, bushwick heirloom woke taiyaki readymade intelligentsia microdosing.</p>",
+          meta_variant: "standard",
+          image_meta: "rich_text_panel/accordion_shelf/None"
+        },
+        id: "fc912b6c-f690-4690-815f-c01d7649ec95"
+      }
+    ],
+    shelf_id: "",
+    image_meta: "accordion_shelf/None/None"
+  };
+
+  let classNamePrefix = 'accordion-shelf';
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AccordionShelf content={content} classNamePrefix={classNamePrefix}/>, div);
+  });
+})
