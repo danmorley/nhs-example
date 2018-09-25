46cec13ca86b127f70b04df48e05a9d98916ce22
Sikander Akbar Ali <sikander.ali@nhs.net>
2018-09-25 10:10:39 +0100

    remove return

--- oneYou2/frontendHandler/models.py
+++ oneYou2/frontendHandler/models.py
@@ -29,7 +29,6 @@ class FrontendVersion:
     @classmethod
     def get_available_versions(cls):
         print('loading available versions')
-        return []
 
         # TODO try to find a way to mock this function on server start
         if settings.AZURE_ACCOUNT_NAME == 'test' or settings.AZURE_ACCOUNT_NAME is None:
