bc3b101c81dee7c900f2e679bead03ae7e4be585
Andrew Kenyon <andrewkenyon123@gmail.com>
2018-09-26 13:49:39 +0100

    update url to live

--- frontend/website-client/src/services/RequestHandler.js
+++ frontend/website-client/src/services/RequestHandler.js
@@ -3,13 +3,13 @@ const ACTION_PLAN_KEY = 'action_plan';
 
 export function signUpForActionPlan() {
   const submission = JSON.parse(sessionStorage.getItem(APF_KEY));
-  submission['ProductToken'] = "3D149395-F755-4586-BA8A-E4F915B023AD";
+  submission['ProductToken'] = "24EE3A12-03E8-4996-BB09-C33573E5E9EA";
   submission['UserItems'] = JSON.parse(sessionStorage.getItem(ACTION_PLAN_KEY));
 
-  return fetch('https://api-test-mentalhealth.cc-testing.co.uk/api/Actions/AddUserRecord', {
+  return fetch('https://api-live-mentalhealth.paragon-cc.co.uk/api/Actions/AddUserRecord', {
       method: 'POST',
       headers : {
-        "Authorization": "paragon-cc:24EE3A12-03E8-4996-BB09-C33573E5E9EA",
+        "Authorization": "3D149395-F755-4586-BA8A-E4F915B023AD",
         "Content-Type": "application/json"
       },
       body: JSON.stringify(submission)
