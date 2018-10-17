72e66163ed4befa3b3c77a7983ed2056aff383ec
David Crees <david.a.crees@gmail.com>
2018-10-17 13:26:48 +0100

    shelf meta_width refinements

--- frontend/website-client/src/components/base/shared/ShelfUtils.js
+++ frontend/website-client/src/components/base/shared/ShelfUtils.js
@@ -6,6 +6,7 @@ class ShelfUtils {
   static shelfContainerClass(shelfContent, defaultClass) {
     if (shelfContent) {
       if (shelfContent.meta_width === 'full') return 'container-fluid';
+      if (shelfContent.meta_width === 'responsive') return 'container';
     }
     return defaultClass || 'container';
   }
