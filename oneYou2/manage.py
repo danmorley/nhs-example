7f073a99b2d00f02c58fa4f723e510f375847fa3
David Crees <david.a.crees@gmail.com>
2018-10-30 18:03:47 +0000

    FIxed settings file name for prod environment

--- oneYou2/manage.py
+++ oneYou2/manage.py
@@ -7,9 +7,10 @@ import sys
 if __name__ == '__main__':
     ENV = os.environ.get('CMS_ENV', 'dev')
     print('MANAGE ENV is {}'.format(ENV))
-    if ENV == 'staging':
-        print('using oneYou2.settings.production for staging')
+    if ENV in ['staging', 'prod']:
+        print('using oneYou2.settings.production for {}'.format(ENV))
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneYou2.settings.production')
+
     else:
         print('using oneYou2.settings.{}'.format(ENV))
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneYou2.settings.{}'.format(ENV))
