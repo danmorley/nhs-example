8550e30630b01eb6389d4affd20bc0130306f145
dmorley <dan_morley@yahoo.com>
2018-12-04 14:16:36 +0000

    replaced # with real URL address in social links

--- frontend/website-client/src/components/oneyou/panels/ShareButtonPanel.js
+++ frontend/website-client/src/components/oneyou/panels/ShareButtonPanel.js
@@ -3,103 +3,64 @@ import CmsComponentRegistry from '../../base/CmsComponentRegistry';
 import './share-button.css';
 import Panel from '../panels/Panel';
 
-const SOCIAL_LINKS = [
-  {
-    share_item: 'email',
-    url: "mailto:?" + "body=" + window.location.href
-  },
-  {
-    share_item: 'whatsapp',
-    url: "whatsapp://send?text=" + " " + window.location.href
-  },
-  {
-    share_item: 'facebook',
-    url: "https://www.facebook.com/sharer/sharer.php?quote=" + "&" + "u=" + window.location.href
-  },
-  {
-    share_item: 'twitter',
-    url: "https://twitter.com/intent/tweet?text=" + "&" + "url=" + window.location.href
-  }
-];
-
-
-class ShareButtonPanel extends Component {
+class ShareButtonPanel extends Component {  
   
   constructor (props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.shareButtonClick = this.shareButtonClick.bind(this);
-    this.socialWindow = this.socialWindow.bind(this);
-  }
-  
-  
-  popup() {
-    var left = (window.screen.width - 570) / 2;
-    var top = (window.screen.height - 570) / 2;
-    var params = 'menubar=no,toolbar=no,status=no,width=570,height=570,top=' + top + ',left=' + left;
-    window.open(this.href, 'NewWindow',params);
-  }
-
-  socialWindow(url) {
-    var left = (window.screen.width - 570) / 2;
-    var top = (window.screen.height - 570) / 2;
-    var params = 'menubar=no,toolbar=no,status=no,width=570,height=570,top=' + top + ',left=' + left;
-    window.open(encodeURI(url), 'NewWindow',params);
   }
 
   setShareButton(elem) {
     this.shareButton = elem;
   }
 
-  handleClick(evt) {
-    let pageUrl = window.location.href,
-      site = '';
-
-    switch (evt.currentTarget.getAttribute('data-social-type')) {
-    case 'facebook':
+  handleClick(evt, item) {    
+    if (item == "email") {
+      return;
+    } 
+    else {
       evt.preventDefault();
-      evt.currentTarget.href = "https://www.facebook.com/sharer/sharer.php?quote=" + "&" + "u=" + pageUrl;
-      this.socialWindow(site);
-      break;
-    case 'twitter':
-      evt.preventDefault();
-      evt.currentTarget.href = "https://twitter.com/intent/tweet?text=" + "&" + "url=" + pageUrl;
-      this.socialWindow(site);
-      break;
-    case 'email':
-      var link = "mailto:?"
-          + "body=" + pageUrl;
-      evt.currentTarget.href=link;
-      break;
-    case 'whatsapp':
-      evt.preventDefault();
-      evt.currentTarget.href = "whatsapp://send?text=" + " " + pageUrl;
-      this.socialWindow(site);
-      break;
-    default:
-      break;
+      let target = evt.target,
+        left = (window.screen.width - 570) / 2,
+        top = (window.screen.height - 570) / 2,
+        params = 'menubar=no,toolbar=no,status=no,width=570,height=570,top=' + top + ',left=' + left;
+        
+      window.open(target.getAttribute('href'), 'NewWindow',params);
     }
   }
-
+  
   render() {
-    let items = SOCIAL_LINKS.map((item, i) => {
-      let left = (window.screen.width - 570) / 2;
-      let top = (window.screen.height - 570) / 2;
-      let link ="<a href= "  + encodeURI(item.url) + " title='(opens in new window)'" 
-              
+    
+    let socialLinks = [
+      {
+        share_item: 'email',
+        url: "mailto:?" + "body=" + window.location.href
+      },
+      {
+        share_item: 'whatsapp',
+        url: "whatsapp://send?text=" + " " + window.location.href
+      },
+      {
+        share_item: 'facebook',
+        url: "https://www.facebook.com/sharer/sharer.php?quote=" + "&" + "u=" + window.location.href
+      },
+      {
+        share_item: 'twitter',
+        url: "https://twitter.com/intent/tweet?text=" + "&" + "url=" + window.location.href
+      }
+    ];
+    
+    let items = socialLinks.map((item, i) => {
+      let link ="<a href= "  + encodeURI(item.url) + " title='(opens in new window)'"           
                 +">"
-                
-                + "<span className =\"screen-reader-text\">" + "share to " + item.share_item + "</span>"
-                
+                + "<span class =\"screen-reader-text\">" + "share to " + item.share_item + "</span>"
                 + "</a>";
-              
-                
+        
       return (
-      <React.Fragment key={i}>
-          <li  className={"share-button__"+item.share_item} key={i} dangerouslySetInnerHTML={{ __html: link }}/>
-          
-          
-       </React.Fragment>
+        <React.Fragment key={i}>
+          <li  className={"share-button__"+item.share_item} key={i} dangerouslySetInnerHTML={{ __html: link }} onClick={(evt) => this.handleClick(evt, item.share_item)} />
+        </React.Fragment>
       );
     });
 
