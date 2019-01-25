b52c2e734d5d9bc7eb087897f8eac8f17b8145b2
Yohan Lebret <yohan.lebret@gmail.com>
2019-01-25 15:27:34 +0000

    Update unittest

--- oneYou2/dctcmsbase/tests.py
+++ oneYou2/dctcmsbase/tests.py
@@ -3,12 +3,12 @@ from oneYou2.test.utils import OneYouTests
 
 
 class BannerModelTests(OneYouTests):
-    def test_meta_layout_returns_the_correct_value(self):
+    def test_heading_value(self):
         banner = create_test_banner()
-        expected_value = 'cta_on_right'
-        self.assertEqual(expected_value, banner.meta_layout)
+        expected_value = 'Test heading'
+        self.assertEqual(expected_value, banner.heading)
 
-    def test_meta_variant_returns_the_correct_value(self):
-        banner = create_test_banner()
-        expected_value = 'how-are-you'
-        self.assertEqual(expected_value, banner.meta_variant)
+#     def test_meta_variant_returns_the_correct_value(self):
+#         banner = create_test_banner()
+#         expected_value = 'how-are-you'
+#         self.assertEqual(expected_value, banner.meta_variant)
